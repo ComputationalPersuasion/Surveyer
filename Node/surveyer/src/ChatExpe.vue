c9ee2a5b5e807c0fc8e80ecd9f4950cae4f074b7
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2018-01-25 11:28:55 +0000

    Rename ChatExpe.

---
+++ Node/surveyer/src/ChatExpe.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <s-survey>
+      <h1 slot="header">Cycling in the city</h1>
+    </s-survey>
+  </div>
+</template>
+
+<script>
+import { SSurvey, SStep } from './components';
+
+export default {
+  name: 'app',
+  components: {
+    SStep,
+    SSurvey,
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
