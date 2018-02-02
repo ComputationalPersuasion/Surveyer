1cc5641c2ff3f15d1a96ada266131deafa3a5af4
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2018-02-02 13:00:08 +0000

    Add SSubmit page.

---
+++ Node/surveyer/src/components/SSubmit.vue
@@ -0,0 +1,37 @@
+<template>
+  <s-question :question="message" v-show="can_show">
+    <slot>
+    </slot>
+  </s-question>
+</template>
+
+<script>
+import { SQuestion } from '.';
+
+export default {
+  name: 's-submit',
+  components: {
+    SQuestion,
+  },
+  data() {
+    return {
+      can_show: false,
+    };
+  },
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    show() {
+      this.can_show = true;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- Node/surveyer/src/components/index.js
+++ Node/surveyer/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as SSurvey } from './SSurvey';
 export { default as SStep } from './SStep';
 export { default as SQuestion } from './SQuestion';
+export { default as SSubmit } from './SSubmit';
