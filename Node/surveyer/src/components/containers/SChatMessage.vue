5d379ca80e375942dc6fa93fe4b475697a7be38a
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2018-02-02 13:05:35 +0000

    Add SChatMessage.

---
+++ Node/surveyer/src/components/containers/SChatMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-chat-message v-if="message.msgs"
+                  :name="message.from"
+                  :text="message.msgs"
+                  :sent="message.sent"
+                  :bg-color="(message.sent ? 'primary' : 'light')"
+                  :text-color="(message.sent ? 'white' : 'black')"
+                  class="animate-fade"
+                  avatar="static/chococat.png"/>
+  <q-chat-message v-else
+                  name="Me"
+                  bg-color="light"
+                  class="animate-fade"
+                  avatar="static/chococat.png">
+    <div class="cursor-pointer" @click="$emit('click')">
+      {{message.text}}
+    </div>
+  </q-chat-message>
+</template>
+
+<script>
+import { QChatMessage } from 'quasar-framework';
+
+export default {
+  name: 's-chat-message',
+  components: {
+    QChatMessage,
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- Node/surveyer/src/components/containers/index.js
+++ Node/surveyer/src/components/containers/index.js
@@ -1,3 +1,4 @@
 export { default as SLikertTable } from './SLikertTable';
 export { default as SChat } from './SChat';
 export { default as SQuestionnaire } from './SQuestionnaire';
+export { default as SChatMessage } from './SChatMessage';
